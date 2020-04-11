13c4887ed9ef827b19114f3b6a0384e5c25f2847
dengmingkeng <645322918@qq.com>
2020-04-11 11:49:06 +0800

    数据填充修改

--- database/seeds/User.php
+++ database/seeds/User.php
@@ -15,7 +15,7 @@ class User extends Seeder
     public function run()
     {
         // faker默认语言是英文会生成英文的数据，在创建实例的时候可以指定为中文
-        $faker = Faker\Factory::create('zh_CN');
-        \app\model\User\User::insert(['account_id'=>1, 'account'=>'admin', 'username'=>'admin', 'sex'=>0, 'phone'=>$faker->phoneNumber, 'email'=>$faker->email, 'role_name'=>'超级管理员']);
+        //$faker = Faker\Factory::create('zh_CN');
+        \app\model\User\User::insert(['account_id'=>1, 'account'=>'admin', 'username'=>'admin', 'sex'=>0, 'role_name'=>'超级管理员']);
     }
 }
