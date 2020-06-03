d5325e5f881120941c2ae8a18e7e2b882cea7201
dengmingkeng <645322918@qq.com>
2020-06-03 11:49:00 +0800

    数据库表迁移，表名修改

--- database/migrations/20200601072903_create_background_message.php
+++ database/migrations/20200601072903_create_background_message.php
@@ -28,7 +28,7 @@ class CreateBackgroundMessage extends Migrator
      */
     public function change()
     {
-        $table = $this->table('backround_message', array('engine'=>'InnoDB'));
+        $table = $this->table('background_message', array('engine'=>'InnoDB'));
         $table->addColumn('type', 'integer', array('length'=>1, 'default'=>1, 'null'=>false, 'comment'=>'消息类别，0：全部发送，1：发给指定用户，2：按条件发送'))
             ->addColumn('title', 'string', array('length'=>100, 'default'=>'', 'null'=>false, 'comment'=>'标题'))
             ->addColumn('content', 'text', array('null'=>false, 'comment'=>'内容'))
